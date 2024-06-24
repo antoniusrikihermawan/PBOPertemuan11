10f4765ef0bbcdfabad0540c44661db15e5c8b1b
A-7 <A-7@A-7>
2024-06-24 10:46:14 +0700

    pbo pertemuan 11 Interface

---
+++ PBOInterface/src/main/java/com/mycompany/pbointerface/Mobil.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbointerface;
+
+/**
+ *
+ * @author A-7
+ */
+class Mobil implements Kendaraan{
+    private String merek;
+    private String model;
+
+    public Mobil(String merek, String model) {
+        this.merek = merek;
+        this.model = model;
+    }
+    
+    public String getMerek() {
+        return merek;
+    }
+    
+    public String getModel() {
+        return model;
+    }
+    
+    public void info() {
+        System.out.println("Mobil : " + merek + " " + model);
+    }   
+}
